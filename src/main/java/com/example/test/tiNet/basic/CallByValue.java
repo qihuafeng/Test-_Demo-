9f72463189439bbbacbaab9432ba5ee39c3f94b3
戚华锋 <2212282402@qq.com>
2020-11-25 15:16:07 +0800

    八大基本类型和其封装,String传参数为传值，其他为传引用，方法处理后改变值

---
+++ src/main/java/com/example/test/tiNet/basic/CallByValue.java
@@ -0,0 +1,59 @@
+package com.example.test.tiNet.basic;
+
+import com.example.test.tiNet.listSortDemo.Test;
+import jdk.nashorn.internal.runtime.linker.LinkerCallSite;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.DeflaterOutputStream;
+
+/**
+ * @Author ShineQi
+ * @Date 2020/11/25 14:59
+ */
+public class CallByValue {
+
+    public  void test(List<Integer> list){
+        if(list.contains(15)){
+          list.remove((Object)15);
+          list.remove((Object)15);
+        }
+        for(int i = 0;i<list.size();i++){
+            if(list.get(i) == 12){
+                list.remove(i);
+            }
+        }
+    }
+
+    public static void  test1(Integer i){
+        i = i+1;
+        System.out.println(i);
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+        list.add(1998);
+        list.add(07);
+        list.add(07);
+        list.add(15);
+        list.add(15);
+        list.add(12);
+        list.add(12);
+        list.add(12);
+
+        CallByValue call = new CallByValue();
+        call.test(list);
+        for(Integer i:list){
+            System.out.println(i);
+        }
+
+        Integer ls = 2;
+        test1(ls);
+        System.out.println(ls);
+    }
+}
